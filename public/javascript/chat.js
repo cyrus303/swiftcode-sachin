51540322181f5c0c9246ac38a1f3d3f3470cc252
cyrus303 <sachinsunny303@gmail.com>
2017-09-16 07:52:40 +0000

    final ui

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,5 +1,26 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController',function ($scope){
+   $scope.messages =[
+   {
+   	   sender: "BOT",
+   	   text:"hello",
+   	   time:"12:00",
+   },
+   {
+       sender :"USER",
+       text:"morning",
+       time:"2:30",
+   }
+   ]
+
+   var  exampleSocket =  new  WebSocket("ws://localhost:9000/chatSocket");
+
+   exampleSocket.onmessage  =   function  (event) {
+          var jsonData = JSON.parse(event.data);
+          console.log(jsonData);
+      };
+
+
 
 });
